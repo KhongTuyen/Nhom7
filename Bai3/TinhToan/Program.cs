393a19a3d4e784512e0017cc36b22598a915ce9c
tuyen2001 <tuyenkhong2001@gmail.com>
2023-03-26 09:34:55 +0700

    Khong Van Tuyen

---
+++ Bai3/TinhToan/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TinhToan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int a, b, c;
+            Console.Write("Nhap vao canh a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap vao canh b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap vao canh c: ");
+
+        }
+    }
+}
