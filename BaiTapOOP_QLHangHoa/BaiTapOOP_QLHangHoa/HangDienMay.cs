d957ddb1eef4f12e6eadd0b64cbb918d859c535f
tuyen2001 <tuyenkhong2001@gmail.com>
2023-04-01 22:24:27 +0700

    Nhom 7 00P

---
+++ BaiTapOOP_QLHangHoa/BaiTapOOP_QLHangHoa/HangDienMay.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapOOP_QLHangHoa
+{
+    class HangDienMay : HangHoa
+    {
+        private Date TGBH = new Date();
+        private int CongSuat;
+
+        public HangDienMay()
+        {
+            CongSuat = 0;
+        }
+        public void NhapHDM()
+        {
+                NhapHangHoa();
+                Console.Write("Nhap Thoi Gian Bao Hanh : ");
+                Console.WriteLine();
+                TGBH.NhapDate();
+                Console.Write("Nhap Cong Suat : ");
+                CongSuat = Convert.ToInt32(Console.ReadLine());
+            
+         }
+
+        public string getMaHang()
+        {
+            return MaHang;
+        }
+        public void XuatHDM()
+        {
+            XuatHangHoa();
+            Console.Write("Thoi Gian Bao Hanh : ");
+            TGBH.XuatDate();
+            Console.Write("Cong Suat : " + CongSuat);
+        }
+
+       
+    }
+
+    
+}
---
+++ BaiTapOOP_QLHangHoa/BaiTapOOP_QLHangHoa/HangHoa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapOOP_QLHangHoa
+{
+     class HangHoa
+    {
+        public  string MaHang;
+        public  string TenHang;
+        private  int SLTon;
+
+        public HangHoa()
+        {
+            MaHang = "";
+            TenHang = "";
+            SLTon = 0;
+        }
+
+        public void NhapHangHoa()
+        {
+            Console.Write("Nhap Ma Hang : ");
+            MaHang = Console.ReadLine();
+            Console.Write("Nhap Ten Hang : ");
+            TenHang = Console.ReadLine();
+            Console.Write("Nhap So Luong Ton : ");
+            SLTon = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void XuatHangHoa()
+        {
+            Console.WriteLine($"Ma Hang : {MaHang}       Ten Hang : {TenHang}       So Luong Ton : {SLTon}");
+        }
+
+        
+    }
+}
